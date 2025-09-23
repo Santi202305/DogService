655f3f8bc047d2ee4ca60bed7144381efbfc0dd7
SANTIAGO GACÍA GRANDA <santiago.garcia09@uceva.edu.co>
2025-09-23 18:38:21 -0500

    Ejercio 1 comple

---
+++ src/test/java/co/edu/uceva/dogservice/DogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uceva.dogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
