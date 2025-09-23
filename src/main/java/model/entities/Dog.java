8322c09cfdec6380fb5c09ad9eec5a36fbcba5a4
SANTIAGO GACÍA GRANDA <santiago.garcia09@uceva.edu.co>
2025-09-23 18:48:26 -0500

    Ejercio 2  comple

---
+++ src/main/java/model/entities/Dog.java
@@ -0,0 +1,28 @@
+package co.empresa.productoservice.model.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private String raza;
+    private Integer edad;
+    private Double peso;
+    private String color;
+    private String sexo; // "Macho" o "Hembra"
+    private Boolean vacunado;
+    private Boolean esterilizado;
+
+}
