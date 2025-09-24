590a2c20d5e29219c6533f32917e05edefdde056
SANTIAGO GACÍA GRANDA <santiago.garcia09@uceva.edu.co>
2025-09-24 08:43:34 -0500

    Ejercio 13 comple

---
+++ src/main/java/co/edu/uceva/dogservice/delivery/exception/GlobalExceptionHandler.java
@@ -0,0 +1,4 @@
+package co.edu.uceva.dogservice.delivery.exception;
+
+public class GlobalExceptionHandler {
+}
---
+++ src/main/java/co/edu/uceva/dogservice/domain/exception/DogNoEncontradoException.java
@@ -0,0 +1,7 @@
+package co.edu.uceva.dogservice.domain.exception;
+
+public class NoHayDogException extends RuntimeException {
+    public NoHayDogException(String message) {
+        super(message);
+    }
+}
